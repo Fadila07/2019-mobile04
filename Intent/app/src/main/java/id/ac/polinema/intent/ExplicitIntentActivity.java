3f1e6cca854b2a5d39fb851b86a191e9608c67f1
Fadila07 <rfadila195@gmail.com>
2019-09-18 14:25:35 +0700

    Explicit Intent

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -9,10 +9,19 @@ import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    private EditText nameInput;
+    private TextView outputName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        nameInput = findViewById(R.id.input_name);
+        outputName = findViewById(R.id.text_output);
     }
 
+
+    public void handleSubmit(View view) {
+        String name = nameInput.getText().toString();
+        outputName.setText("Hello " +name +", Congratulations!");
+    }
 }
