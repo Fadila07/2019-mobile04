2d8970218c6ccbe886b55096e8a67399ac1589dc
Fadila07 <rfadila195@gmail.com>
2019-09-18 17:29:33 +0700

    Parcelable

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -2,9 +2,23 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.widget.TextView;
+
+import id.ac.polinema.intent.model.User;
+
+import static id.ac.polinema.intent.BundleActivity.NAME_KEY;
+import static id.ac.polinema.intent.BundleActivity.USERNAME_KEY;
 
 public class ProfileParcelableActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,11 +26,20 @@ public class ProfileParcelableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_parcelable);
 
         // TODO: bind here
-
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-
+            User user = getIntent().getParcelableExtra("User");
             // TODO: display value here
+            String username = user.getUsername();
+            String name = user.getName();
+            Integer age = user.getAge();
+
+            usernameText.setText(username);
+            nameText.setText(name);
+            ageText.setText(String.valueOf(age));
         }
     }
 }
